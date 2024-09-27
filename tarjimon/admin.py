aa63d2cda16016e1280db5523025eebc070ebb84
sardorbekesanov <164501111+sardorbekesanov@users.noreply.github.com>
2024-09-27 23:23:36 +0500

    loyiha deploying

---
+++ tarjimon/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Lugat
+# Register your models here.
+
+
+class LugatAdmin(admin.ModelAdmin):
+
+    list_display=['inglizcha', 'uzbekcha']
+
+admin.site.register(Lugat, LugatAdmin)
